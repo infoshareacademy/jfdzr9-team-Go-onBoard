792d8fb3bdc337e2fc12f5a32814995ddec1e95b
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-03-24 15:33:04 +0100

    adding working layout of dashboard

--- src/components/HomePageComponents/WelcomeContainer.jsx
+++ src/components/HomePageComponents/WelcomeContainer.jsx
@@ -5,32 +5,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export const WelcomeContainer = () => {
-  const [userName, setUserName] = useState([]);
-
-  const getUserName = async () => {
-    const userCollerction = collection(database, "users");
-    const querySnapshot = await getDocs(userCollerction);
-    const users = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    setUserName(users);
-  };
-
-  useEffect(() => {
-    getUserName();
-  }, []);
-
   return (
     <div className="greetingsContainer">
-      <h2>
-        Hej, {""}
-        {userName.map(({ id, name }) => (
-          <span key={id}>{name}!</span>
-        ))}
-      </h2>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe fuga, iure sunt enim distinctio accusamus. Aliquam hic minima a, possimus nobis dicta delectus. Provident odio
-      officiis voluptas alias soluta est?
+      <h2>Hej,</h2>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe fuga, iure sunt enim distinctio accusamus. Aliquam hic minima a, possimus nobis dicta delectus. Provident
+        odio officiis voluptas alias soluta est?
+      </div>
     </div>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,22 @@
 import "./../index.css";
 import { WelcomeContainer } from "../components/HomePageComponents/WelcomeContainer";
+import { StagesContainer } from "../components/HomePageComponents/StagesContainer";
 
 export const HomePageLayout = () => {
   return (
-    <div className="layoutConatiner">
-      <WelcomeContainer />
-    </div>
+    <>
+      <div className="up-container">
+        <WelcomeContainer />
+        <div className="calendar">
+          <h1>Calendar under construction</h1>
+        </div>
+      </div>
+      <div className="down-container">
+        <StagesContainer />
+        <div className="buddy">
+          <h1>Buddy acess under construction</h1>
+        </div>
+      </div>
+    </>
   );
 };
