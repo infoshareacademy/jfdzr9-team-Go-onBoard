786506caab9eb4024ff21904f5fc2093ea1f37df
Vladyslav Kh <vladislavhrabryk@gmail.com>
2023-03-26 12:40:51 +0200

    create panel login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Signin } from "../src/components/RequireAuth/Signin";
+import { Signup } from "../src/components/RequireAuth/Signup";
 
 function App() {
   return (
     <AuthContextProvider>
       <Routes>
-        <Route
-          path="/"
-          element={<Signin />}
-        />
-        <Route
-          path="/signup"
-          element={<Signup />}
-        />
-        <Route
-          path="/signpassword"
-          element={<Signpassword />}
-        />
-        <Route
-          path="/account"
-          element={<Account />}
-        />
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signpassword" element={<Signpassword />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </AuthContextProvider>
   );
