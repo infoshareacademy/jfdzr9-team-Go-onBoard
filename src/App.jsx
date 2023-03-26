cfc8b11eacc224ac139363da3e1b4be18d534600
Vladyslav Kh <vladislavhrabryk@gmail.com>
2023-03-26 09:16:19 +0200

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import Signin from "./components/RequireAuth/Signin";
 import Signup from "./components/RequireAuth/Signup";
 import Signpassword from "./components/RequireAuth/Signpassword";
 import { HomePageLayout } from "./pages/Homepage";
+import { AuthContextProvider } from "./components/RequireAuth/context/AuthContext";
+import Account from "./components/RequireAuth/Account";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signpassword" element={<Signpassword />} />
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signpassword" element={<Signpassword />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </AuthContextProvider>
   );
 }
 
