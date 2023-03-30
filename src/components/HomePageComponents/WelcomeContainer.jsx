c718337ef677e3803d72cecee673a45d72e2deff
Kamil Chwesiuk <kamil.chwesiuk@gmail.com>
2023-03-30 23:33:53 +0200

    modified mapping method into distructure in WelcomeContainer

--- src/components/HomePageComponents/WelcomeContainer.jsx
+++ src/components/HomePageComponents/WelcomeContainer.jsx
@@ -3,23 +3,16 @@ import { useFirebaseFetch } from "../hooks/useFirebaseFetch";
 
 export const WelcomeContainer = () => {
   // using hook useFirebaseFetch() to fetch collection called "users"//
-  const userName = useFirebaseFetch("users");
+  const [userName] = useFirebaseFetch("users");
+  const { name } = userName || {};
   // using hook useFirebaseFetch() to fetch collection called "courses"//
-  const greetingText = useFirebaseFetch("courses");
+  const [greetingText] = useFirebaseFetch("courses");
+  const { greeting } = greetingText || {};
 
   return (
     <div className="greetingsContainer">
-      <h2>
-        Hej, {""}
-        {userName.map(({ id, name }) => (
-          <span key={id}>{name}!</span>
-        ))}
-      </h2>
-      <div>
-        {greetingText.map(({ id, greeting }) => (
-          <p key={id}>{greeting}</p>
-        ))}
-      </div>
+      <h2>Hej,{name}!</h2>
+      <div>{greeting}</div>
     </div>
   );
 };
