a4071b9eb962412d3d9680532098812efc32d1a0
pompajana <99181930+pompajana@users.noreply.github.com>
2023-04-02 14:27:46 +0200

    Admin add to inituser

--- src/adminControl/AddUser.tsx
+++ src/adminControl/AddUser.tsx
@@ -1,6 +1,12 @@
-import { useRef, FormEvent } from "react";
+import { useRef, FormEvent, useState } from "react";
 import { database } from "../utils/firebase/firebase.config";
-import { collection, doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  Timestamp,
+  FieldValue,
+} from "firebase/firestore";
 
 interface User {
   email: string;
@@ -8,49 +14,89 @@ interface User {
   gender: string;
   id_course: string;
   role: string;
-  start_course: string;
+  start_course: Timestamp | FieldValue;
 }
 
-function AddActivity() {
+function AddUser() {
   const emailRef = useRef<HTMLInputElement>(null);
   const usernameRef = useRef<HTMLInputElement>(null);
-  const genderRef = useRef<HTMLInputElement>(null);
+  const genderRef = useRef<HTMLSelectElement>(null);
   const id_courseRef = useRef<HTMLInputElement>(null);
-  const roleRef = useRef<HTMLInputElement>(null);
+  const roleRef = useRef<HTMLSelectElement>(null);
   const start_courseRef = useRef<HTMLInputElement>(null);
+  const [message, setMessage] = useState<string | null>(null);
 
   // Push user to table
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const usersRef = collection(database, "initUser");
+    const start_course = start_courseRef.current?.value
+      ? Timestamp.fromDate(new Date(start_courseRef.current.value))
+      : serverTimestamp();
     const newUser: User = {
       email: emailRef.current?.value || "",
       name: usernameRef.current?.value || "",
       gender: genderRef.current?.value || "",
       id_course: id_courseRef.current?.value || "",
       role: roleRef.current?.value || "",
-      start_course: start_courseRef.current?.value || "",
+      start_course: start_course,
     };
-    setDoc(doc(usersRef), newUser).catch(alert);
+    addDoc(usersRef, newUser)
+      .then(() => {
+        setMessage("Dodano użytownika");
+        setTimeout(() => setMessage(null), 2000); // clear message after 2 seconds
+      })
+      .catch(() => setMessage("Error"));
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="E-mail">E-mail</label>
-      <input ref={emailRef} />
-      <label htmlFor="Imię kursanta">Imię kursanta</label>
-      <input ref={usernameRef} />
-      <label htmlFor="Płeć">Płeć</label>
-      <input ref={genderRef} />
-      <label htmlFor="Sygnatura kursu">Sygnatura kursu</label>
-      <input ref={id_courseRef} />
-      <label htmlFor="Rola">Rola</label>
-      <input ref={roleRef} />
-      <label htmlFor="Rozpoczęcie kursu">Rozpoczęcie kursu</label>
-      <input ref={start_courseRef} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form
+        onSubmit={onSubmit}
+        style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="E-mail">E-mail</label>
+        <input
+          ref={emailRef}
+          type="email"
+          required
+        />
+        <label htmlFor="Imię kursanta">Imię kursanta</label>
+        <input
+          ref={usernameRef}
+          required
+        />
+        <label htmlFor="Płeć">Płeć</label>
+        <select
+          ref={genderRef}
+          required>
+          <option value="">Wybierz płeć</option>
+          <option value="M">M</option>
+          <option value="K">K</option>
+        </select>
+        <label htmlFor="Sygnatura kursu">Sygnatura kursu</label>
+        <input
+          ref={id_courseRef}
+          required
+        />
+        <label htmlFor="Rola">Rola</label>
+        <select
+          ref={roleRef}
+          required>
+          <option value="">Wybierz rolę</option>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+        <label htmlFor="Rozpoczęcie kursu">Rozpoczęcie kursu</label>
+        <input
+          ref={start_courseRef}
+          type="datetime-local"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
   );
 }
 
-export default AddActivity;
+export default AddUser;
