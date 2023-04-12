84fff66492deb8fa214dbbc83da464d340e755e9
pompajana <99181930+pompajana@users.noreply.github.com>
2023-04-12 21:51:37 +0200

    time to course calculate

--- src/components/HomePageComponents/Calendar.tsx
+++ src/components/HomePageComponents/Calendar.tsx
@@ -1,17 +1,38 @@
-import { useState, useEffect } from "react";
+import "../../index.css";
+import { useUser } from "../RequireAuth/context/AuthContext";
 import { useFirebaseFetch } from "../hooks/useFirebaseFetch";
 
-interface StartCourseData {
-  startCourse: string | null;
+import { Timestamp } from "firebase/firestore";
+import { FieldValue } from "firebase/firestore";
+
+interface IUser {
+  uid: string;
+  start_course: Timestamp | FieldValue;
+}
+
+// Type guard function
+function isTimestamp(value: Timestamp | FieldValue): value is Timestamp {
+  return value instanceof Timestamp;
 }
 
 function Calendar() {
-  // using hook useFirebaseFetch() to fetch collection called "users"//
-  const [startCourseData] = useFirebaseFetch<StartCourseData>("users");
+  const userName = useUser();
+  const usersCollection = useFirebaseFetch<IUser>("users");
 
-  const { startCourse } = startCourseData || {};
+  const currentUser = usersCollection?.find(
+    (user) => user.uid === userName?.uid
+  );
 
-  return <h2>Hej, {startCourse}!</h2>;
+  const now = new Date();
+  let daysUntilStart: string | number = "Nieznany";
+
+  if (currentUser?.start_course && isTimestamp(currentUser.start_course)) {
+    const start_course_date = currentUser.start_course.toDate();
+    const timeDifference = start_course_date.getTime() - now.getTime();
+    daysUntilStart = Math.ceil(timeDifference / (1000 * 3600 * 24));
+  }
+
+  return <h2>Kurs rozpocznie się za: {daysUntilStart} dni!</h2>;
 }
 
 export default Calendar;
