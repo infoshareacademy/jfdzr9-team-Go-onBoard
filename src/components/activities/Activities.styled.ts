672e692199f1aa6b23d7dc534cf3f427056496ab
pompajana <99181930+pompajana@users.noreply.github.com>
2023-04-28 19:54:23 +0200

    mobile style

--- src/components/activities/Activities.styled.ts
+++ src/components/activities/Activities.styled.ts
@@ -10,6 +10,10 @@ export const ActivitiesContainer = styled.div`
   box-shadow: rgb(0 0 0 / 15%) 4px 5px 10px;
   border-radius: 31.2294px;
   height: 380px;
+  @media only screen and (max-width: 1020px) {
+    height: 300px;
+    padding: 15px;
+  }
 `;
 export const ActivitiName = styled.div`
   font-family: "Inter";
@@ -26,6 +30,8 @@ export const Container = styled.div`
   gap: 2rem;
   @media only screen and (max-width: 1020px) {
     flex-direction: column;
+    align-items: center;
+    gap: 1rem;
   }
 `;
 export const EtapContainer = styled.div`
@@ -74,7 +80,7 @@ export const DetailsWraper = styled.div`
   box-shadow: rgba(0, 0, 0, 0.15) 4px 5px 10px;
   border-radius: 31.2294px;
 
-  @media only screen and (max-width: 850px) {
+  @media only screen and (max-width: 950px) {
     width: 500px;
   }
 
@@ -96,6 +102,9 @@ export const StyledLinkFetchedHeader = styled.div`
   gap: 4px;
   filter: brightness(0) saturate(100%) invert(9%) sepia(34%) saturate(5579%)
     hue-rotate(234deg) brightness(90%) contrast(121%);
+  @media only screen and (max-width: 950px) {
+    padding: 6px 2px 6px 6px;
+  }
 `;
 
 export const HeaderInfo = styled.div`
