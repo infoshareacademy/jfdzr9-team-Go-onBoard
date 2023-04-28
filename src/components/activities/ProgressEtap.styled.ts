6cb9939aba0f53dc04c363cebdd8a899896d3982
pompajana <99181930+pompajana@users.noreply.github.com>
2023-04-28 08:48:50 +0200

    progress etap style

---
+++ src/components/activities/ProgressEtap.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export const ProgressWraper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  margin: 30px auto -65px;
+`;
+
+export const DonutChart = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
