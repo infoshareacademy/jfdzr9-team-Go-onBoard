a70b8c3b8e83ef0267917630932a004e948e9313
pompajana <99181930+pompajana@users.noreply.github.com>
2023-05-06 08:39:00 +0200

    trzymanie po przeładowaniu

--- src/components/RequireAuth/context/AuthContext.tsx
+++ src/components/RequireAuth/context/AuthContext.tsx
@@ -18,15 +18,21 @@ const UserContext = createContext<IUserContext>({
 
 export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<IUserContext["user"]>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setIsLoading(false);
     });
     return () => {
       unsubscribe();
     };
   }, []);
+
+  if (isLoading) {
+    return <div>Ładowanie...</div>;
+  }
 
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
